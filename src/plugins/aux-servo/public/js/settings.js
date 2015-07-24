873f492596b53eb3d27fd590bc4a66d749daccde
codewithpassion <dominik.fretz@gmail.com>
2015-07-24 16:46:12 -0700

    Fix path issues

--- src/plugins/aux-servo/public/js/settings.js
+++ src/plugins/aux-servo/public/js/settings.js
@@ -3,12 +3,12 @@ auxServoNs.Settings = function() {
   'use strict';
   var self = this;
   self.get = function (servo, callback) {
-    $.get('/plugin/aux-servo/config/' + servo, function (config) {
+    $.get('plugin/aux-servo/config/' + servo, function (config) {
       if (callback != undefined)
         callback(config);
     });
   };
   self.set = function (servo, config) {
-    $.post('/plugin/aux-servo/config/' + servo, config);
+    $.post('plugin/aux-servo/config/' + servo, config);
   };
 };
